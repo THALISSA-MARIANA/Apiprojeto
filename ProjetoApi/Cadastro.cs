107fa584e22d896e1e697cfa637dc652cf5b8077
Thalissa <thalissamariana02@gmail.com>
2022-09-26 22:31:35 -0300

    first commit

---
+++ ProjetoApi/Cadastro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjetoApi
+{
+    public class Cadastro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Cliente")]
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public int idade { get; set; }
+
+        public string E-mail { get; set; }
+
+        public int Cep { get; set; }
+
+         public string Logradouro { get; set; }
+
+         public int Numero { get; set; }
+
+         public string Complemento { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public ICollection<>? UF { get; set; }
+
+        public bool Sexo { get; set; }
+
+        public int RG { get; set; }
+
+        public int CPF { get; set; }
+
+        public ICollection<>? Orgao Expedidor{ get; set; }
+
+        public bool CNH { get; set; }
+
+        public bool Fumante { get; set; }
+
+        public bool CursoCuidador { get; set; }
+
+        public int CargaHoraria { get; set; }
+
+        public ICollection<>? EscolhaCursos { get; set; }
+
+        public int CorenEnfermagem { get; set; }
+    }
+}
---
+++ ProjetoApi/Candidatura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjetoApi
+{
+    public class Candidatura
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("TipoAnuncio")]
+        public int AnuncioId { get; set; }
+        public TipoAnuncio? Anuncio { get; set; }
+
+        [ForeignKey("Candidato")]
+        public int CandidatoId { get; set; }
+        public Cliente? Candidato { get; set; }
+
+        public Enum Status { get; set; }
+
+        public ICollection<HistoricoCandidatura>? Historico { get; set; }
+    }
+}
